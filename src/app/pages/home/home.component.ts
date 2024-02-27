7b5470f08b1e7d2219cf9970a8b9c171ce2e2494
rymnd0800 <rymnd08@gmail.com>
2024-02-28 00:57:13 +0800

    refactor code and computation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,17 +2,17 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { NavCardComponent } from '../../components/nav-card/nav-card.component';
 import { RouterModule } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, FormsModule } from '@angular/forms';
 import { assessmentLevel } from '../../shared/assessmentLevel';
-import { Land } from '../../shared/ILand.interface';
-import { Classes } from '../../shared/ILand.interface';
-import { agriLand, urbandLand } from '../../shared/data';
+import { Classes, Land, LandData } from '../../shared/ILand.interface';
+import { landData as data } from '../../shared/data';
 
 interface computedLand{
+  id: number,
   land: string
   classType: string
   area: number
-  fairmarketValue: string
+  fairMarketValue: string
   marketValue2003: number
   marketValue2011: number
   assessedValue2003: number
@@ -20,6 +20,7 @@ interface computedLand{
 }
 
 interface formValue {
+  area: number
   land: number | string
   classType: number | string
   fairMarketValue: number | string
@@ -35,70 +36,103 @@ interface formValue {
 export class HomeComponent implements OnInit {
   //properties
   landForm!: FormGroup
-  landData: Land[] = []
-  agriLand: Land[] = []
-  selectedClass: Classes[] = []
+  landData: LandData[] = []
+  selectedLandType!: LandData | null
+  selectedLand: Land | null = null
+  selectedClass: Classes | null = null
+
+
   assessmentLvl: assessmentLevel[] = [] 
   computedLand: computedLand[] = []
-
   constructor( private fb: FormBuilder ){}
 
   ngOnInit(): void {
     this.initializeForm()
-    this.landData = urbandLand
     this.assessmentLvl = assessmentLevel
-    this.agriLand = agriLand
+    this.landData = data
+
   }
 
   //methods
   resetData(){
-    this.initializeForm()
+    this.resetForm()
     this.computedLand = []
   }
 
-  handleLandChange(){
-    const landSelectValue = this.landForm.get('land')?.value
+  resetForm(){
+    this.selectedLandType = null
+    this.selectedLand = null
+    this.selectedClass = null
+    this.initializeForm()
+  }
 
-    const getClass = this.agriLand?.find(land => land.id == landSelectValue)?.classes 
-    this.selectedClass = getClass as Classes[]
+  handleLandTypeChange(event: Event){
+    this.selectedLand = null
+
+    const value = (event.target as HTMLSelectElement).value
+    const selectedLandType = this.landData.find(land => land.id == value)
+    this.selectedLandType = selectedLandType ? selectedLandType : null
+
+  }
+
+  handleLandChange(event: Event){
+    if(this.selectedLandType){
+      const selectedLand = this.selectedLandType.data.find(land => land.id == (event.target as HTMLSelectElement).value)
+      this.selectedLand = selectedLand ? selectedLand : null
+    }
+  }
+  
+  handleClassChange(event: Event){
+    if(this.selectedLand){
+      const selectedClass = this.selectedLand.classes.find(_class => _class.id == (event.target as HTMLSelectElement).value)
+      this.selectedClass = selectedClass ? selectedClass : null
+
+      console.log(this.selectedClass)
+    }
+  }
+
+  deleteLandRow(id: number){
+    this.computedLand = this.computedLand.filter(land => land.id !== id)
   }
 
   handleSubmit(){
-    const formValue = this.landForm.value
+
+    const formValue = this.landForm.value as formValue
     console.log(formValue)
-    // area 
     const AREA = formValue.area
+    const FMV = this.assessmentLvl.find(ass => ass.id == formValue.fairMarketValue)
 
-    // land 
-    const selectedLand = this.agriLand.find(land => land.id == formValue.land)
-    const land = selectedLand?.landName
-    console.log(land)
+    if(this.selectedLandType && this.selectedLand && this.selectedClass && FMV){
+      const MV_2003 = this.selectedClass.marketValue2003
+      const MV_2011 = this.selectedClass.marketValue2011
+      
+      const ASS_2003 = FMV.assessmentLevel2003
+      const ASS_2011 = FMV.assessmentLevel2011
 
-    // class 
-    const selectedClass = selectedLand?.classes.find(className => className.id == formValue.classType)
-    const classType = selectedClass?.className
-    const MARKET_VALUE_2003 = selectedClass?.marketValue2003
-    const MARKET_VALUE_2011 = selectedClass?.marketValue2011
+      const computed_mv_2003 = AREA * MV_2003
+      const computed_mv_2011 = AREA * MV_2011
 
-    //fair market value
-    const selectedFairMarketValue = this.assessmentLvl.find(assLvl => assLvl.id == formValue.fairMarketValue)
-    const fairMarketValue = selectedFairMarketValue?.name
-    const ASSESSMENT_LVL_2003 = selectedFairMarketValue?.assessmentLevel2003
-    const ASSESSMENT_LVL_2011 = selectedFairMarketValue?.assessmentLevel2011
+      const computed_av_2003 = computed_mv_2003 * ASS_2003
+      const computed_av_2011 = computed_mv_2011 * ASS_2011
 
-    // computation
-    if(MARKET_VALUE_2003 && MARKET_VALUE_2011 && ASSESSMENT_LVL_2003 && ASSESSMENT_LVL_2011){
-      const marketValue2003 = AREA * MARKET_VALUE_2003
-      const marketValue2011 = AREA * MARKET_VALUE_2011
-      const assessedValue2003 = marketValue2003 * ASSESSMENT_LVL_2003
-      const assessedValue2011 = marketValue2011 * ASSESSMENT_LVL_2011
-
-      this.computedLand.push({land: land,classType: classType, area: AREA, fairmarketValue: fairMarketValue, marketValue2003, marketValue2011, assessedValue2003, assessedValue2011} as computedLand)
+      this.computedLand.push(
+        {
+          id: Date.now(),
+          land: this.selectedLand.landName, 
+          classType: this.selectedClass.className, 
+          area: AREA, 
+          fairMarketValue: FMV.name, 
+          marketValue2003: computed_mv_2003, 
+          marketValue2011: computed_mv_2011, 
+          assessedValue2003: computed_av_2003,
+          assessedValue2011: computed_av_2011
+        }
+      )
+      
     }
 
-
     // form reset 
-    this.initializeForm()
+    this.resetForm()
   }
 
   get totalMarketValue(){
@@ -118,10 +152,11 @@ export class HomeComponent implements OnInit {
 
   initializeForm(){
     this.landForm = this.fb.group({
+      landType: ['', Validators.required],
       land: ['', Validators.required],
       classType: ['', Validators.required],
+      area: ['', Validators.required],
       fairMarketValue: ['', Validators.required],
-      area: ['', Validators.required]
     })
   }
 
--- src/app/shared/ILand.interface.ts
+++ src/app/shared/ILand.interface.ts
@@ -1,12 +1,19 @@
 export interface Classes {
-  id?: number | string;
+  id: number | string;
   className: string;
   marketValue2003: number;
   marketValue2011: number;
 }
 
 export interface Land {
-  id?: string | number;
+  id: string | number;
   landName: string;
   classes: Classes[];
 }
+
+export interface LandData {
+  id: string | number
+  landType: string
+  data: Land[]
+}
+
